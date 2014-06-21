d453622ee6d84fdff6a4f00ee86a5ceac512e0c3
Ivan Fraixedes <ifcdev@gmail.com>
2014-06-21 22:08:29 +0100

    Chrome-extension implementation v.0.0.1

--- scripts/toc-widget.js
+++ scripts/toc-widget.js
@@ -1,44 +1,49 @@
 'use strict';
 
-//query to append the div element for the TOC '.repository-sidebar.clearfix'
+(function (window, document) {
+  var TOC_DIV_ID = 'chrome-extension-github-readme-toc';
+  var rightSideCtnElem;
+  var readmeAElems;
+  var tocUlElem;
+  var tocHeader;
+  var tocCtnElem = document.getElementById(TOC_DIV_ID);
+  var readmeElem = document.querySelector('#readme');
 
-var TOC_DIV_ID = 'chrome-extension-github-readme-toc';
-var rightSideCtnElem;
-var readmeAElems;
-var tocUlElem;
-var tocCtnElem = document.getElementById(TOC_DIV_ID);
-var readmeElem = document.querySelector('#readme');
+  if (readmeElem) {
+    if (!tocCtnElem) {
+      tocCtnElem = document.createElement('div');
+      tocCtnElem.setAttribute('id', TOC_DIV_ID);
+      tocCtnElem.classList.add('toc-widget-ctn');
 
-if (readmeElem) {
-  if (!tocCtnElem) {
-    tocCtnElem = document.createElement('div');
-    tocCtnElem.setAttribute('id', TOC_DIV_ID);
-    tocCtnElem.classList.add('toc-widget-ctn');
+      rightSideCtnElem = document.querySelector('.repository-sidebar.clearfix');
+      readmeAElems = readmeElem.querySelectorAll('[href^="#"]');
 
-    rightSideCtnElem = document.querySelector('.repository-sidebar.clearfix');
-    readmeAElems = readmeElem.querySelectorAll('[href^="#"]');
+      if (readmeAElems) {
+        tocUlElem = document.createElement('ul');
+        tocCtnElem.appendChild(tocUlElem);
+        tocHeader = document.createElement('li');
+        tocHeader.innerHTML = 'Table of contents';
+        tocUlElem.appendChild(tocHeader);
 
-    if (readmeAElems) {
-      tocUlElem = document.createElement('ul');
-      tocCtnElem.appendChild(tocUlElem);
+        [].forEach.call(readmeAElems, function (readmeElem, idx) {
+          var tocLiElem = document.createElement('li');
+          var tocAElem = document.createElement('a');
 
-      [].forEach.call(readmeAElems, function (readmeElem, idx) {
-        var tocLiElem = document.createElement('li');
-        var tocAElem = document.createElement('a');
+          tocAElem.innerHTML = readmeElem.parentElement.textContent.trim();
+          tocAElem.setAttribute('href', readmeElem.getAttribute('href'));
+          tocLiElem.setAttribute('title', tocAElem.innerHTML);
+          tocLiElem.appendChild(tocAElem);
+          tocUlElem.appendChild(tocLiElem);
+        });
 
-        tocAElem.innerHTML = readmeElem.parentElement.textContent.trim();
-        tocAElem.setAttribute('href', readmeElem.getAttribute('href'));
-        tocLiElem.appendChild(tocAElem);
-        tocUlElem.appendChild(tocLiElem);
-      });
-
-      rightSideCtnElem.appendChild(tocCtnElem);
-      tocCtnElem.classList.add('show');
+        rightSideCtnElem.appendChild(tocCtnElem);
+        tocCtnElem.classList.add('show');
+      }
+    }
+  } else {
+    if (tocCtnElem) {
+      tocCtnElem.classList.remove('show');
     }
   }
-} else {
-  if (tocCtnElem) {
-    tocCtnElem.classList.remove('show');
-  }
-}
+})(window, document);
 
